7abbf97e5d3dd4c4a0588efab2a46920a828bb71
Betryz <beatryz.f@aluno.ifsp.edu.br>
2024-11-20 09:38:04 -0300

    add

--- backend/src/controllers/movies/getMovieByIdController.js
+++ backend/src/controllers/movies/getMovieByIdController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { getMovieByIdModel } from "../../models/moviesModel.js";
 
 export const getMovieById = async (req, res) => {
@@ -14,20 +13,3 @@ export const getMovieById = async (req, res) => {
         res.status(500).json({ message: 'Erro ao buscar dados do TMDB' });
     }
 };
-=======
-import { getMovieByIdModel } from "../../models/moviesModel.js";
-
-export const getMovieById = async (req, res) => {
-    const { id } = req.params;
-    
-    try {
-
-        const movie = await getMovieByIdModel(+id);
-
-        res.json(movie);
-    } catch (error) {
-        console.error('Erro ao buscar o filme pelo ID no TMDB:', error.message);
-        res.status(500).json({ message: 'Erro ao buscar dados do TMDB' });
-    }
-};
->>>>>>> master
